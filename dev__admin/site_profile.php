2c550bf536d1f0963cf1526b75800ff28d0fe218
phpbala <32deva@gmail.com>
2017-11-04 17:16:59 +0530

    Mysqli implemented version changes
    Mysqli implemented version changes

--- dev__admin/site_profile.php
+++ dev__admin/site_profile.php
@@ -4,22 +4,22 @@
 </div>
 <?php echo (isset($_REQUEST['Make']))? admin::make_default_run($_REQUEST) : ""; ?>
 <br />
-        <form method="post">
-        <div id="make_default">
-        <ul>
-        <?php $make = admin::make_default();
-		$r = select(DB_PREFIX."pages where default_page=1");
-		$get = fetch($r);
-		while($view = fetch($make))
-		{
-		 ?>
-         <li><input type="radio" name="default" <?php if($get->page_id==$view->page_id){?>checked="checked"<?php } ?> value="<?php echo $view->page_id; ?>" />&nbsp;&nbsp;<?php echo $view->page_title; ?></li>
-        <?php } //make default has pending ?>
-        </ul>
-        </div>       
-        <span style="float:left; margin:5px 0;"><input type="submit" name="Make" class="Button" value="Make Default" /></span>
-        </form>
-        </div>
+<form method="post">
+<div id="make_default">
+<ul>
+<?php $make = admin::make_default();
+$r = db()->query("select * from ".DB_PREFIX."pages where default_page=1");
+$get = $r->fetch_object();
+while($view = $make->fetch_object())
+{
+ ?>
+ <li><input type="radio" name="default" <?php if($get->page_id==$view->page_id){?>checked="checked"<?php } ?> value="<?php echo $view->page_id; ?>" />&nbsp;&nbsp;<?php echo $view->page_title; ?></li>
+<?php } //make default has pending ?>
+</ul>
+</div>       
+<span style="float:left; margin:5px 0;"><input type="submit" name="Make" class="Button" value="Make Default" /></span>
+</form>
+</div>
 
     
     
