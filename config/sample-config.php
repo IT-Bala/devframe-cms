0bef4595d232111a9c85f95a36c9604534adade1
phpbala <32deva@gmail.com>
2017-11-04 15:43:53 +0530

    Mysqli implemented version changes
    Mysqli implemented version changes

--- config/sample-config.php
+++ config/sample-config.php
@@ -5,10 +5,4 @@ define('USERNAME','');
 define('PASSWORD','');
 define('DATABASE','');	
 define('DB_PREFIX','');						   
-class DB{
-      function __construct(){
-		  $sql = mysql_connect(HOSTNAME,USERNAME,PASSWORD);
-		  mysql_select_db(DATABASE,$sql);
-	  }
-}
-new DB;
+$db = new MySqli(HOSTNAME,USERNAME,PASSWORD,DATABASE);
