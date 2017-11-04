f7c51e2c7ff8c5b9b70f940c5d10912ab6e0bbc0
phpbala <32deva@gmail.com>
2017-11-04 15:46:46 +0530

    Mysqli implemented version changes
    Mysqli implemented version changes

--- dev__admin/page_load.php
+++ dev__admin/page_load.php
@@ -4,16 +4,16 @@ include('frame/dev__.php');
 if(isset($_REQUEST['page'])){
         if($_REQUEST['page'] != ''){
 	    if(isset($_SESSION['page_id'])){
-	      query("update ".DB_PREFIX."pages set page_title='".$_REQUEST['page']."' where page_id=".$_SESSION['page_id']."");
+	      db()->query("update ".DB_PREFIX."pages set page_title='".$_REQUEST['page']."' where page_id=".$_SESSION['page_id']."");
 		}else{
 		// insert
-		$que = query("insert into ".DB_PREFIX."pages(page_title,page_date,status) values ('".$_REQUEST['page']."',now(),1)");
-	    $ID = mysql_insert_id();
+		$que = db()->query("insert into ".DB_PREFIX."pages(page_title,page_date,status) values ('".$_REQUEST['page']."',now(),1)");
+	    $ID = db()->insert_id();
 		$_SESSION['page_id']=$ID; 
 		// select
 		$slt = where(DB_PREFIX."pages","page_id=".$_SESSION['page_id']."");
-		$link = fetch($slt);
-		query("update ".DB_PREFIX."pages set page_link='page.php?page_id=".$link->page_id."' where page_id=".$_SESSION['page_id']."");
+		$link = $slt->fetch_object();
+		db()->query("update ".DB_PREFIX."pages set page_link='page.php?page_id=".$link->page_id."' where page_id=".$_SESSION['page_id']."");
 		}
 		echo "<strong>Page Link :&nbsp;&nbsp;</strong><input type='text' readonly='readonly' value='page.php?page_id=".$_SESSION['page_id']."' /><br><br>";
 		}
@@ -21,9 +21,3 @@ if(isset($_REQUEST['page'])){
 	    
 }
 ?>
-
-
-    
-    
-    
-        
