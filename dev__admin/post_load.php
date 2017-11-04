d0ae9e92f634332c2ec2b609321d09c5dfceca2c
phpbala <32deva@gmail.com>
2017-11-04 15:47:24 +0530

    Mysqli implemented version changes
    Mysqli implemented version changes

--- dev__admin/post_load.php
+++ dev__admin/post_load.php
@@ -5,16 +5,16 @@ if(isset($_REQUEST['page']))
 {
         if($_REQUEST['page'] != ''){
 	    if(isset($_SESSION['post_id'])){
-	      query("update ".DB_PREFIX."posts set post_title='".$_REQUEST['page']."' where post_id=".$_SESSION['post_id']."");
+	    	db()->query("update ".DB_PREFIX."posts set post_title='".$_REQUEST['page']."' where post_id=".$_SESSION['post_id']."");
 		}else{
 		// insert
-		$que = query("insert into ".DB_PREFIX."posts(post_title,post_date,status) values ('".$_REQUEST['page']."','".$url."',now(),1)");
-	    $ID = mysql_insert_id();
+		$que = db()->query("insert into ".DB_PREFIX."posts(post_title,post_date,status) values ('".$_REQUEST['page']."','".$url."',now(),1)");
+	    $ID = db()->insert_id();
 		$_SESSION['post_id']=$ID;
 		// select
 		$slt = where(DB_PREFIX."posts","post_id=".$_SESSION['post_id']."");
-		$link = fetch($slt);
-		query("update ".DB_PREFIX."posts set post_link='page.php?p_id=".$link->post_id."' where post_id=".$_SESSION['post_id']."");
+		$link = $slt->fetch_object();
+		db()->query("update ".DB_PREFIX."posts set post_link='page.php?p_id=".$link->post_id."' where post_id=".$_SESSION['post_id']."");
 		}
 		echo "<strong>Post Link :&nbsp;&nbsp;</strong><input type='text' readonly='readonly' value='page.php?p_id=".$_SESSION['post_id']."' /><br><br>";
 		}
@@ -22,9 +22,3 @@ if(isset($_REQUEST['page']))
 	    
 }
 ?>
-
-
-    
-    
-    
-        
